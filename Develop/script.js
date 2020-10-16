036aa361b44ddee69b34bce34cd1219d5bac0ad4
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-10-15 17:13:59 -0700

    added arrays

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var lowerCase = ("a", "b" , "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 
 var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",);
 
-var
+var nums = ("1" "2" "3" "4" "5" "6" "7" "8" "9")
 
 // Write password to the #password input
 function writePassword() {
