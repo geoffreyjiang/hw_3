888b181234cda4fe0c4278a7629dd5bfc43a20e2
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-10-16 19:56:14 -0700

    added confirms

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,10 @@ var specialChar = ["#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","
 var length = parseInt(prompt("How long do you want your password to be?"));
 
 if(length < 8 || length > 128){
-  alert("Your password is too short or too long!")
+  alert("Your password must be 8 - 128 characters long!")
 }
+
+var useLower = confirm("Do you want to include lowercase characters?");
+var useUpperCase=confirm("Do you want to include uppercase characters?");
+var useNumeric=confirm("Do you want to include numeric characters?");
+var useSpecial=confirm("Do you want to include special characters?");
