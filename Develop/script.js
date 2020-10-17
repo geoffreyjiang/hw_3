2cb5c267f2f9ef2795c27d588cdf3446e5a39f36
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-10-16 22:34:58 -0700

    done

--- Develop/script.js
+++ Develop/script.js
@@ -1,60 +1,39 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 var lowerCase = ("a", "b" , "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-
 var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-
 var nums = ("1", "2", "3", "4", "5", "6", "7", "8", "9");
 var specialChar = ["#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","["];
-
+var pwArea = document.querySelector("#thepassword")
 
 function generatePassword(){
-  var passwordString =  "";
-  
+  var pw =  "";
+  var possibles = []
   var length = parseInt(prompt("How long do you want your password to be?"));
 
   if(length < 8 || length > 128 || isNaN(length)){
     alert("Your password must be 8 - 128 characters long!")
+  } else {
+
+  var lowercaseTrue = confirm("Do you want to include lowercase characters?");
+  var upperCaseTrue=confirm("Do you want to include uppercase characters?");
+  var numsTrue=confirm("Do you want to include numbers?");
+  var specialCharTrue=confirm("Do you want to include special characters?");
+
+
+
+  if (lowercaseTrue)  possibles.push(lowerCase) 
+  if (upperCaseTrue)  possibles.push(upperCase) 
+  if (numsTrue) { possibles.push(nums) }
+  if (specialCharTrue) { possibles.push(specialChar) }
+
+while (pw.length < length) {
+    for (let i = 0; i < possibles.length; i++){
+    let rand = Math.floor(Math.random() * possibles[i].length)
+    pw += possibles[i][rand]
+    }
+
+pwArea.textContent = pw
+}
   }
-
-// Declaring variables
-var useLower = confirm("Do you want to include lowercase characters?");
-var useUpper=confirm("Do you want to include uppercase characters?");
-var useNums=confirm("Do you want to include numbers?");
-var useSpecial=confirm("Do you want to include special characters?");
-
-
-
-// Check to see if all variables are included 
-while (useLower == false && useUpper == false && useNums == false && useSpecial == false) { 
-  alert("Your password did not meet requirements")
-  useLower=confirm("Do you want to include lowercase characters?");
-  useUpper=confirm("Do you want to include uppercase characters?");
-  useNums=confirm("Do you want to include numbers?");
-  useSpecial=confirm("Do you want to include special characters?");
-}
-
-for (let i = 0; passwordLength;i++)
-var randomCharArrayNum;
-var selectedCharArray;
-var randomCharNum;
-var randomChar;
-
-randomCharArrayNum= parseInt(Math.floor(Math.random()*caseArray.length)); 
-
-selectedCharArray=caseArray[randomCharArrayNum]; 
-
-randomCharNum=Math.floor(Math.random()*selectedCharArray.length);
-
-randomChar=selectedCharArray[randomCharNum];
-
-passwordString+=randomChar;
-
-
-
-}
-
-
-
-
-
+}  
+document.querySelector("#generate").addEventListener('click', generatePassword)
