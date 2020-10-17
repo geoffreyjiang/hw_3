99f173578f5dba3b522dd37e1670c6e70c12bc22
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-10-17 09:40:49 -0700

    final touches

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,8 @@
 // Assignment Code
-var lowerCase = ("a", "b" , "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-var nums = ("1", "2", "3", "4", "5", "6", "7", "8", "9");
-var specialChar = ["#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","["];
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var nums = '0123456789';
+var specialChar = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
 var pwArea = document.querySelector("#thepassword")
 
 function generatePassword(){
